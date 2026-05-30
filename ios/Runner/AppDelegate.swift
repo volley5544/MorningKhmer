012a67a58f79d6838b04aa4d5470669d6baf22d2
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-05-30 04:29:39 +0000

    Updating to latest FlutterFlow output.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyCWpkuiUYJK2H36LmjsrGGM3hNgfy6a50k")
+    GMSServices.provideAPIKey("AIzaSyCpV73gOXIj52Hr8UjuYTcPNoxtOLLMyG8")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
