5f71c3a2922e4dfc84ad77eb41f691defc9a2b75
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-06-02 07:13:53 +0000

    Updating to latest FlutterFlow output.

--- lib/backend/schema/user_custom_record.dart
+++ lib/backend/schema/user_custom_record.dart
@@ -45,6 +45,16 @@ class UserCustomRecord extends FirestoreRecord {
   String get profileImage => _profileImage ?? '';
   bool hasProfileImage() => _profileImage != null;
 
+  // "fcm_token" field.
+  String? _fcmToken;
+  String get fcmToken => _fcmToken ?? '';
+  bool hasFcmToken() => _fcmToken != null;
+
+  // "access_token" field.
+  String? _accessToken;
+  String get accessToken => _accessToken ?? '';
+  bool hasAccessToken() => _accessToken != null;
+
   void _initializeFields() {
     _employeeId = snapshotData['employee_id'] as String?;
     _createdDate = snapshotData['created_date'] as DateTime?;
@@ -52,6 +62,8 @@ class UserCustomRecord extends FirestoreRecord {
     _email = snapshotData['email'] as String?;
     _phoneNumber = snapshotData['phone_number'] as String?;
     _profileImage = snapshotData['profile_image'] as String?;
+    _fcmToken = snapshotData['fcm_token'] as String?;
+    _accessToken = snapshotData['access_token'] as String?;
   }
 
   static CollectionReference get collection =>
@@ -95,6 +107,8 @@ Map<String, dynamic> createUserCustomRecordData({
   String? email,
   String? phoneNumber,
   String? profileImage,
+  String? fcmToken,
+  String? accessToken,
 }) {
   final firestoreData = mapToFirestore(
     <String, dynamic>{
@@ -104,6 +118,8 @@ Map<String, dynamic> createUserCustomRecordData({
       'email': email,
       'phone_number': phoneNumber,
       'profile_image': profileImage,
+      'fcm_token': fcmToken,
+      'access_token': accessToken,
     }.withoutNulls,
   );
 
@@ -120,7 +136,9 @@ class UserCustomRecordDocumentEquality implements Equality<UserCustomRecord> {
         e1?.uid == e2?.uid &&
         e1?.email == e2?.email &&
         e1?.phoneNumber == e2?.phoneNumber &&
-        e1?.profileImage == e2?.profileImage;
+        e1?.profileImage == e2?.profileImage &&
+        e1?.fcmToken == e2?.fcmToken &&
+        e1?.accessToken == e2?.accessToken;
   }
 
   @override
@@ -130,7 +148,9 @@ class UserCustomRecordDocumentEquality implements Equality<UserCustomRecord> {
         e?.uid,
         e?.email,
         e?.phoneNumber,
-        e?.profileImage
+        e?.profileImage,
+        e?.fcmToken,
+        e?.accessToken
       ]);
 
   @override
---
+++ lib/custom_code/firestore_stream_manager.dart
@@ -0,0 +1,101 @@
+import 'dart:async';
+
+import 'package:arun_sawad/app_state.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../auth/base_auth_user_provider.dart';
+import '../backend/schema/user_custom_record.dart';
+import '/app_events/index.dart';
+
+class FirestoreStreamManager {
+  FirestoreStreamManager._();
+  static final FirestoreStreamManager instance = FirestoreStreamManager._();
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  /// ----------------------------
+  /// Stream Controllers
+  /// ----------------------------
+
+  final StreamController<QuerySnapshot?> _userProfileController =
+      StreamController<QuerySnapshot?>.broadcast();
+
+  /// Public streams
+
+  Stream<QuerySnapshot?> get userProfileStream => _userProfileController.stream;
+
+  /// ----------------------------
+  /// Firestore subscriptions
+  /// ----------------------------
+
+  StreamSubscription<QuerySnapshot>? _userProfileSubscription;
+
+  /// ----------------------------
+  /// Start listeners
+  /// ----------------------------
+
+  void start() {
+    print('start listenProfile :');
+    _startUserProfileListener();
+  }
+
+  void stop() {
+    print('stopped listenProfile :');
+    _userProfileSubscription?.cancel();
+  }
+
+  /// ============================================================
+  /// PRIVATE METHODS
+  /// ============================================================
+
+  void _startUserProfileListener() {
+    /// Prevent duplicate listener
+    _userProfileSubscription?.cancel();
+
+    _userProfileSubscription = _firestore
+        .collection('user_custom')
+        .where('employee_id', isEqualTo: '${FFAppState().employeeID}')
+        .snapshots()
+        .listen((docSnapshot) async {
+      if (docSnapshot.docs.isNotEmpty) {
+        print('user_custom : ${docSnapshot.docs.first.data()!}');
+        print('token : ${docSnapshot.docs.first.data()!['access_token']}');
+        FFAppState().accessTokenFirestore =
+            '${docSnapshot.docs.first.data()!['access_token']}';
+        if (loggedIn) {
+          if ('${docSnapshot.docs.first.data()!['access_token']}' != '' &&
+              '${docSnapshot.docs.first.data()!['access_token']}' != 'null') {
+            if ('${FFAppState().accessToken}' != '' &&
+                '${FFAppState().accessToken}' != 'null' &&
+                '${FFAppState().accessToken}' != 'access_token') {
+              if (FFAppState().isProductionNew) {
+                if ('${docSnapshot.docs.first.data()!['access_token']}' !=
+                    '${FFAppState().accessToken}') {
+                  print('Force Logout 5544');
+                  FFAppEventService.instance.triggerAppEvent(
+                    ForceLogoutEventEvent(
+                      timestamp: DateTime.now(),
+                      waitForCompletion: true,
+                      debugId: '3579',
+                    ),
+                  );
+                }
+              }
+            }
+          } else {
+            if ('${FFAppState().accessToken}' != '' &&
+                '${FFAppState().accessToken}' != 'null' &&
+                '${FFAppState().accessToken}' != 'access_token') {
+              if ('${FFAppState().userRef!}' != '' &&
+                  '${FFAppState().userRef!}' != 'null') {
+                // update token in firebase
+                await FFAppState().userRef!.update(createUserCustomRecordData(
+                      accessToken: FFAppState().accessToken,
+                    ));
+              }
+            }
+          }
+        }
+      }
+    });
+  }
+}
